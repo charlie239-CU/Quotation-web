c13ec0b3f59458626bdad8dad1e8711cc45941a2
vivek rawat <rawatvivek239@gmail.com>
2021-03-12 22:44:54 +0530

    refined index.html

---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+  $colors = array("red", "green", "blue", "yellow","purple","black");
+    foreach ($colors as $value) {
+        echo "<p>$value</p> <br>";
+      }
+      echo "count:+++asdadad++".count($colors);
+      ?>
